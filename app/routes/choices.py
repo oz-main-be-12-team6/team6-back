081b0cd6c23079550491a4d3b536abfe54790656
zzinypark <zzinypark21@gamil.com>
2025-07-21 15:48:53 +0900

    questions 수정

--- app/routes/choices.py
+++ app/routes/choices.py
@@ -6,8 +6,7 @@ choices_blp = Blueprint("choices", __name__, url_prefix="/choices")
 
 @choices_blp.route("/question/<int:question_id>", methods=["GET"])
 def get_choices_by_question(question_id):
-    choices = Choices.query.filter_by(question_id=question_id).order_by(Choices.sqe).all()
+    choices = Choices.query.filter_by(question_id=question_id, is_active=True).order_by(Choices.sqe).all()
 
     result = [choice.to_dict() for choice in choices]
-
     return jsonify({"choices": result})
